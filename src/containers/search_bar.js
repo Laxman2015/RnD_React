499785dfa1fb9eb5b6529fe57ed6dcf974c33f6e
Laxman2015 <lxmn2010@gmail.com>
2018-10-01 10:19:33 +0530

    redux change need

---
+++ src/containers/search_bar.js
@@ -0,0 +1,34 @@
+import React,{Component} from 'react';
+export default class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      term:''
+    }
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event){
+    this.setState({term:event.target.value})
+  }
+  onFormSubmit(event){
+      event.preventDefault();
+  }
+
+  render()
+    {return(
+    <form onSubmit={this.onFormSubmit} className="input-group">
+      <input 
+      placeholder="Enter City to forcost 5 day weather"
+      className="form-control"
+      value={this.state.term} 
+      onInput={this.onInputChange}
+      />
+      <span className="input-group-btn">
+        <button type="submit" className="btn btn-secondary">Submit</button>
+      </span>
+    </form>
+  )
+  }
+}
